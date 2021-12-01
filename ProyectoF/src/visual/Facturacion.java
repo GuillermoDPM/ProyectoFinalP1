bb8195080d359f55757215e9af31cd7bc1f5d54d
GuillermoDPM <guillermod24@hotmail.com>
2021-12-01 16:26:18 -0400

    Facturacion modificada

--- ProyectoF/src/visual/Facturacion.java
+++ ProyectoF/src/visual/Facturacion.java
@@ -7,13 +7,30 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import Logi.Usuario;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
 
 public class Facturacion extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtFactura;
+	private JTextField txtNombreUsuario;
+	private JTextField txtDireccionUsuario;
+	private JTextField txtTelefono;
+	private JTextField txtLogin;
+	private JTextField txtUsername;
+	private JTextField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -32,38 +49,123 @@ public class Facturacion extends JDialog {
 	 * Create the dialog.
 	 */
 	public Facturacion() {
-		setBounds(100, 100, 450, 300);
+		setModal(true);
+		setResizable(false);
+		setTitle("Facturacion");
+		setBounds(100, 100, 573, 490);
+		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		{
-			JLabel lblNewLabel = new JLabel("Factura #");
-			lblNewLabel.setBounds(10, 11, 48, 14);
-			contentPanel.add(lblNewLabel);
-		}
-		{
-			txtFactura = new JTextField();
-			txtFactura.setBounds(68, 8, 86, 20);
-			contentPanel.add(txtFactura);
-			txtFactura.setColumns(10);
-		}
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Informacion Usuario", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(12, 13, 531, 383);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(12, 27, 56, 16);
+		panel.add(lblNewLabel);
+		
+		txtNombreUsuario = new JTextField();
+		txtNombreUsuario.setBounds(90, 24, 366, 20);
+		panel.add(txtNombreUsuario);
+		txtNombreUsuario.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Direccion:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(12, 71, 68, 16);
+		panel.add(lblNewLabel_1);
+		
+		txtDireccionUsuario = new JTextField();
+		txtDireccionUsuario.setColumns(10);
+		txtDireccionUsuario.setBounds(90, 67, 366, 20);
+		panel.add(txtDireccionUsuario);
+		
+		JLabel lblNewLabel_2 = new JLabel("Telefono:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(12, 112, 68, 16);
+		panel.add(lblNewLabel_2);
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setColumns(10);
+		txtTelefono.setBounds(90, 108, 366, 20);
+		panel.add(txtTelefono);
+		
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLogin.setBounds(12, 152, 50, 16);
+		panel.add(lblLogin);
+		
+		txtLogin = new JTextField();
+		txtLogin.setColumns(10);
+		txtLogin.setBounds(90, 148, 366, 20);
+		panel.add(txtLogin);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUsername.setBounds(12, 181, 79, 16);
+		panel.add(lblUsername);
+		
+		txtUsername = new JTextField();
+		txtUsername.setColumns(10);
+		txtUsername.setBounds(90, 177, 366, 20);
+		panel.add(txtUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPassword.setBounds(12, 210, 68, 16);
+		panel.add(lblPassword);
+		
+		txtPassword = new JTextField();
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(90, 206, 366, 20);
+		panel.add(txtPassword);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			
+			JButton btnModificar = new JButton("Modificar");
+			btnModificar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+				}
+			});
+			buttonPane.add(btnModificar);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btnRegistrar = new JButton("Registrar");
+				btnRegistrar.setActionCommand("OK");
+				btnRegistrar.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Usuario aux = new Usuario(txtNombreUsuario.getText(), txtDireccionUsuario.getText(), txtTelefono.getText(), txtLogin.getText(), txtUsername.getText(), txtPassword.getText());
+						JOptionPane.showMessageDialog(null, "Registro Satisfactorio", "Información", JOptionPane.INFORMATION_MESSAGE);
+					}
+				});
+				buttonPane.add(btnRegistrar);
+				getRootPane().setDefaultButton(btnRegistrar);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
+				JButton cancelButton = new JButton("Cancelar");
 				cancelButton.setActionCommand("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						int option = JOptionPane.showConfirmDialog(null, "¿Realmente desea cancelar el registro del cliente?","Confirmar",JOptionPane.INFORMATION_MESSAGE);
+						if(JOptionPane.YES_OPTION == option) {
+							dispose();
+						}
+						
+						
+					}
+				});
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
