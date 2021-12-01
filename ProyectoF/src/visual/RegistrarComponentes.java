119b9b06996232e29536b859f775be91c4812208
GuillermoDPM <guillermod24@hotmail.com>
2021-12-01 13:43:38 -0400

    Agregando Clase Login y Facturacion. Modificando la vista de RegisComponentes

--- ProyectoF/src/visual/RegistrarComponentes.java
+++ ProyectoF/src/visual/RegistrarComponentes.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
@@ -55,111 +57,16 @@ public class RegistrarComponentes extends JFrame {
 		setResizable(false);
 		setTitle("Registro Componentes");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 653, 891);
+		setBounds(100, 100, 553, 489);
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Informacion general", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(12, 13, 269, 124);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Codigo:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel.setBounds(12, 36, 56, 16);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_2 = new JLabel("Marca:");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(12, 78, 56, 16);
-		panel.add(lblNewLabel_2);
-		
-		textField = new JTextField();
-		textField.setBounds(68, 34, 166, 22);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(68, 76, 166, 22);
-		panel.add(textField_1);
-		
-		JPanel JPMemoriaRam = new JPanel();
-		JPMemoriaRam.setBorder(new TitledBorder(null, "Memoria Ram", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		JPMemoriaRam.setBounds(12, 236, 469, 75);
-		contentPane.add(JPMemoriaRam);
-		JPMemoriaRam.setLayout(null);
-		
-		JLabel lblNewLabel_3 = new JLabel("Capacidad (MB):");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_3.setBounds(12, 34, 117, 16);
-		JPMemoriaRam.add(lblNewLabel_3);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(129, 32, 117, 22);
-		JPMemoriaRam.add(spinner);
-		
-		JLabel lblNewLabel_4 = new JLabel("Tipo:");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_4.setBounds(266, 36, 56, 16);
-		JPMemoriaRam.add(lblNewLabel_4);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "DDR1", "DDR2", "DDR3", "DDR4"}));
-		comboBox_1.setBounds(307, 34, 117, 22);
-		JPMemoriaRam.add(comboBox_1);
-		
-		JPanel JPMicroprocesador = new JPanel();
-		JPMicroprocesador.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Microprocesador", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		JPMicroprocesador.setBounds(12, 347, 469, 114);
-		contentPane.add(JPMicroprocesador);
-		JPMicroprocesador.setLayout(null);
-		
-		JLabel lblNewLabel_5 = new JLabel("Modelo:");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_5.setBounds(23, 38, 57, 16);
-		JPMicroprocesador.add(lblNewLabel_5);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(75, 36, 382, 22);
-		JPMicroprocesador.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblNewLabel_6 = new JLabel("Conector:");
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_6.setBounds(12, 78, 68, 16);
-		JPMicroprocesador.add(lblNewLabel_6);
-		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Intel", "AMD"}));
-		comboBox_2.setBounds(75, 76, 124, 22);
-		JPMicroprocesador.add(comboBox_2);
-		
-		JLabel lblNewLabel_7 = new JLabel("Velocidad(Mhz):");
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_7.setBounds(223, 79, 234, 16);
-		JPMicroprocesador.add(lblNewLabel_7);
-		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setBounds(333, 76, 124, 22);
-		JPMicroprocesador.add(spinner_1);
-		
-		JButton btnNewButton = new JButton("REGISTRAR");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		btnNewButton.setBounds(351, 65, 138, 41);
-		contentPane.add(btnNewButton);
-		
 		JPanel JPMotherboard = new JPanel();
 		JPMotherboard.setBorder(new TitledBorder(null, "Motherboard", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		JPMotherboard.setBounds(12, 487, 553, 124);
+		JPMotherboard.setBounds(12, 249, 484, 136);
 		contentPane.add(JPMotherboard);
 		JPMotherboard.setLayout(null);
 		
@@ -205,8 +112,105 @@ public class RegistrarComponentes extends JFrame {
 		
 		JRadioButton rdbtnSata_2 = new JRadioButton("SATA-3");
 		rdbtnSata_2.setHorizontalAlignment(SwingConstants.CENTER);
-		rdbtnSata_2.setBounds(383, 61, 109, 23);
+		rdbtnSata_2.setBounds(385, 62, 79, 23);
 		JPMotherboard.add(rdbtnSata_2);
+		// BORRAR
+		JPMotherboard.setVisible(false);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Informacion general", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(12, 13, 269, 124);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Codigo:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(12, 36, 56, 16);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Marca:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(12, 78, 56, 16);
+		panel.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(68, 34, 166, 22);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(68, 76, 166, 22);
+		panel.add(textField_1);
+		
+		JPanel JPMemoriaRam = new JPanel();
+		JPMemoriaRam.setBorder(new TitledBorder(null, "Memoria Ram", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		JPMemoriaRam.setBounds(12, 249, 484, 136);
+		contentPane.add(JPMemoriaRam);
+		JPMemoriaRam.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("Capacidad (MB):");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(12, 34, 117, 16);
+		JPMemoriaRam.add(lblNewLabel_3);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(129, 32, 117, 22);
+		JPMemoriaRam.add(spinner);
+		
+		JLabel lblNewLabel_4 = new JLabel("Tipo:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_4.setBounds(266, 36, 56, 16);
+		JPMemoriaRam.add(lblNewLabel_4);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "DDR1", "DDR2", "DDR3", "DDR4"}));
+		comboBox_1.setBounds(307, 34, 117, 22);
+		JPMemoriaRam.add(comboBox_1);
+		
+		JPanel JPMicroprocesador = new JPanel();
+		JPMicroprocesador.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Microprocesador", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		JPMicroprocesador.setBounds(12, 249, 484, 136);
+		contentPane.add(JPMicroprocesador);
+		JPMicroprocesador.setLayout(null);
+		
+		JLabel lblNewLabel_5 = new JLabel("Modelo:");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_5.setBounds(23, 38, 57, 16);
+		JPMicroprocesador.add(lblNewLabel_5);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(75, 36, 382, 22);
+		JPMicroprocesador.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Conector:");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_6.setBounds(12, 78, 68, 16);
+		JPMicroprocesador.add(lblNewLabel_6);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Intel", "AMD"}));
+		comboBox_2.setBounds(75, 76, 124, 22);
+		JPMicroprocesador.add(comboBox_2);
+		
+		JLabel lblNewLabel_7 = new JLabel("Velocidad(Mhz):");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_7.setBounds(223, 79, 234, 16);
+		JPMicroprocesador.add(lblNewLabel_7);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(333, 76, 124, 22);
+		JPMicroprocesador.add(spinner_1);
+		
+		JButton btnNewButton = new JButton("REGISTRAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnNewButton.setBounds(384, 36, 138, 41);
+		contentPane.add(btnNewButton);
 		
 		JPanel JPComponentes = new JPanel();
 		JPComponentes.setBorder(new TitledBorder(null, "Componentes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -216,7 +220,7 @@ public class RegistrarComponentes extends JFrame {
 		
 		JPanel JPDiscoDuro = new JPanel();
 		JPDiscoDuro.setBorder(new TitledBorder(null, "Disco Duro", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		JPDiscoDuro.setBounds(12, 624, 469, 166);
+		JPDiscoDuro.setBounds(12, 249, 484, 136);
 		contentPane.add(JPDiscoDuro);
 		JPDiscoDuro.setLayout(null);
 		
@@ -226,7 +230,7 @@ public class RegistrarComponentes extends JFrame {
 		JPDiscoDuro.add(lblModelo_1);
 		
 		textField_6 = new JTextField();
-		textField_6.setBounds(260, 27, 86, 20);
+		textField_6.setBounds(179, 28, 86, 20);
 		textField_6.setColumns(10);
 		JPDiscoDuro.add(textField_6);
 		
@@ -243,7 +247,20 @@ public class RegistrarComponentes extends JFrame {
 		lblTipoDeConexin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblTipoDeConexin.setBounds(10, 61, 132, 14);
 		JPDiscoDuro.add(lblTipoDeConexin);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(179, 58, 86, 20);
+		JPDiscoDuro.add(textField_5);
+		textField_5.setColumns(10);
+		JPMicroprocesador.setVisible(false);
+		JPMemoriaRam.setVisible(true);
+		JPDiscoDuro.setVisible(false);
+		//BORRAR
+		
+		
+		
 		JRadioButton rdbtnMemoriaRam = new JRadioButton("Memoria Ram");
+		rdbtnMemoriaRam.setSelected(true);
 		rdbtnMemoriaRam.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		rdbtnMemoriaRam.setBounds(6, 25, 119, 23);
 		rdbtnMemoriaRam.setHorizontalAlignment(SwingConstants.CENTER);
@@ -269,10 +286,18 @@ public class RegistrarComponentes extends JFrame {
 		rdbtnDiscoDuro.setBounds(414, 25, 109, 23);
 		JPComponentes.add(rdbtnDiscoDuro);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(520, 727, 86, 20);
-		contentPane.add(textField_5);
-		textField_5.setColumns(10);
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int option = JOptionPane.showConfirmDialog(null, "¿Realmente desea cancelar el registro del componente?","Confirmar",JOptionPane.INFORMATION_MESSAGE);
+				if(JOptionPane.YES_OPTION == option) {
+					dispose();
+				}
+			}
+		});
+		btnCancelar.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnCancelar.setBounds(384, 408, 138, 41);
+		contentPane.add(btnCancelar);
 		
 		rdbtnDiscoDuro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
